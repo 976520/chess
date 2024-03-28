1142825a96edfaa3204c10f8beccf2101df12bba
송재욱 <123460320+976520@users.noreply.github.com>
2024-03-29 02:10:36 +0900

    fix kill log bug

--- src/main.py
+++ src/main.py
@@ -288,6 +288,27 @@ class Game:
                     sys.exit()
                 elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
                     return
+                
+    def display_kill_log(self):
+        x_offset = 750
+        y_offset = 100 + (min(len(self.kill_log), 6) - 1) * 100 
+        for killer_piece, killed_piece in self.kill_log[-5:]:  
+            killer_image = self.piece_images[type(killer_piece).__name__ + '_' + killer_piece.color[0]]
+            killed_image = self.piece_images[type(killed_piece).__name__ + '_' + killed_piece.color[0]]
+            self.screen.blit(killer_image, (x_offset, y_offset))
+            start_pos = (x_offset + 80, y_offset + 40)
+            end_pos = (x_offset + 110, y_offset + 40)  
+            angle = np.arctan2(end_pos[1] - start_pos[1], end_pos[0] - start_pos[0])
+            arrow_size = 10
+            arrow_points = [
+                (end_pos[0] + 10, end_pos[1]), 
+                (end_pos[0] - arrow_size * np.cos(angle - np.pi / 4), end_pos[1] - arrow_size * np.sin(angle - np.pi / 4)),
+                (end_pos[0] - arrow_size * np.cos(angle + np.pi / 4), end_pos[1] - arrow_size * np.sin(angle + np.pi / 4))
+            ]
+            pygame.draw.line(self.screen, (255, 0, 0), start_pos, end_pos, 5)
+            pygame.draw.polygon(self.screen, (255, 0, 0), arrow_points)
+            self.screen.blit(killed_image, (x_offset + 120, y_offset))
+            y_offset -= 100  
     
     class MCTSNode:
         def __init__(self, state, parent=None, action=None):
@@ -488,6 +509,8 @@ class Game:
             if action:
                 (start_row, start_col), move = action
 
+                if self.board.board[move[0], move[1]] is not None:
+                    self.kill_log.append((self.board.board[start_row, start_col], self.board.board[move[0], move[1]]))
 
                 self.board.board[move[0], move[1]] = self.board.board[start_row, start_col]
                 self.board.board[start_row, start_col] = None
@@ -561,26 +584,7 @@ class Game:
                         score -= value
         return score
 
-    def display_kill_log(self):
-        x_offset = 750
-        y_offset = 100 + (min(len(self.kill_log), 6) - 1) * 100 
-        for killer_piece, killed_piece in self.kill_log[-5:]:  
-            killer_image = self.piece_images[type(killer_piece).__name__ + '_' + killer_piece.color[0]]
-            killed_image = self.piece_images[type(killed_piece).__name__ + '_' + killed_piece.color[0]]
-            self.screen.blit(killer_image, (x_offset, y_offset))
-            start_pos = (x_offset + 80, y_offset + 40)
-            end_pos = (x_offset + 110, y_offset + 40)  
-            angle = np.arctan2(end_pos[1] - start_pos[1], end_pos[0] - start_pos[0])
-            arrow_size = 10
-            arrow_points = [
-                (end_pos[0] + 10, end_pos[1]), 
-                (end_pos[0] - arrow_size * np.cos(angle - np.pi / 4), end_pos[1] - arrow_size * np.sin(angle - np.pi / 4)),
-                (end_pos[0] - arrow_size * np.cos(angle + np.pi / 4), end_pos[1] - arrow_size * np.sin(angle + np.pi / 4))
-            ]
-            pygame.draw.line(self.screen, (255, 0, 0), start_pos, end_pos, 5)
-            pygame.draw.polygon(self.screen, (255, 0, 0), arrow_points)
-            self.screen.blit(killed_image, (x_offset + 120, y_offset))
-            y_offset -= 100  
+
 
 def main_menu():
     pygame.init()

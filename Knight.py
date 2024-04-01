c567e6911e91542e9a7adeac761f50e49d1b8a17
송재욱 <123460320+976520@users.noreply.github.com>
2024-04-01 22:22:18 +0900

    style

--- Knight.py
+++ Knight.py
@@ -8,6 +8,6 @@ class Knight(Piece):
         for d in directions:
             new_pos = position + d
             if np.all((0 <= new_pos) & (new_pos < 8)):
-                if board[new_pos[0], new_pos[1]] is None or board[new_pos[0], new_pos[1]].color != self.color:
+                if (board[new_pos[0], new_pos[1]] is None) or (board[new_pos[0], new_pos[1]].color != self.color):
                     moves.append(tuple(new_pos))
         return moves

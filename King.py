a54f919486aa77bd33ec1d40280959c69e7eea07
송재욱 <123460320+976520@users.noreply.github.com>
2024-04-02 22:25:38 +0900

    style

--- King.py
+++ King.py
@@ -14,18 +14,23 @@ class King(Piece):
         for d in directions:
             new_pos = position + d
             if np.all((0 <= new_pos) & (new_pos < 8)):
-                if board[new_pos[0], new_pos[1]] is None or board[new_pos[0], new_pos[1]].color != self.color:
+                if (board[new_pos[0], new_pos[1]] is None) or (board[new_pos[0], new_pos[1]].color != self.color):
                     moves.append(tuple(new_pos))
 
         if not self.has_moved:
-            row = 7 if self.color == 'white' else 0
+            if self.color == 'white':
+                row = 7
+            elif self.color == 'black':
+                row = 0
 
-            if isinstance(board[row, 7], Rook) and not board[row, 7].has_moved:
-                if np.all(board[row, 5:7] == None):
-                    moves.append((row, 6))
+            if isinstance(board[row, 7], Rook):
+                if not board[row, 7].has_moved:
+                    if np.all(board[row, 5:7] == None):
+                        moves.append((row, 6))
 
-            if isinstance(board[row, 0], Rook) and not board[row, 0].has_moved:
-                if np.all(board[row, 1:4] == None):
-                    moves.append((row, 2))
+            if isinstance(board[row, 0], Rook):
+                if not board[row, 0].has_moved:
+                    if np.all(board[row, 1:4] == None):
+                        moves.append((row, 2))
 
         return moves

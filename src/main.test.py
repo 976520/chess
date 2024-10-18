212d79e187799805e23553e0f851b4ae28010a04
송재욱 <123460320+976520@users.noreply.github.com>
2024-10-18 12:24:14 +0900

    testcode

--- src/main.test.py
+++ src/main.test.py
@@ -1,10 +1,13 @@
 import unittest
 from main import Game
+import pygame
 
 class TestGameInitialization(unittest.TestCase):
     def setUp(self):
+        pygame.init()
         self.game = Game()
-
+        self.screen = pygame.display.get_surface()
+        
     def test_default_initialization(self):
         self.assertEqual(self.game.current_turn, 'white')
         self.assertIsNone(self.game.selected_piece)
@@ -18,6 +21,15 @@ class TestGameInitialization(unittest.TestCase):
     def test_initial_turn(self):
         game = Game()
         self.assertEqual(game.current_turn, 'white')
+        
+    def test_handle_mousebuttondown(self):
+        pygame.mouse.set_pos(150, 150) 
+        pygame.event.post(pygame.event.Event(pygame.MOUSEBUTTONDOWN, pos=(150, 150)))
 
+        self.game.handle_events()
+        self.assertEqual(self.game.selected_piece, self.game.board.board[1, 1])
+        self.assertEqual(self.game.selected_position, (1, 1))
+
+        
 if __name__ == '__main__':
     unittest.main()

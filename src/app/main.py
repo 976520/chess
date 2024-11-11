14d0ecbff2d122ba81d3e648ca9b56c5634577d6
송재욱 <haensol976520@gmail.com>
2024-11-12 08:56:13 +0900

    del-unuse

--- src/app/main.py
+++ src/app/main.py
@@ -3,8 +3,6 @@ import sys
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from features.game.Game import Game
-
 if __name__ == "__main__":
     from pages.Menu import Menu
     Menu().run()

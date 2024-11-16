9276cdd1379a113720aafefa3ffa73b89623e37b
송재욱 <haensol976520@gmail.com>
2024-11-16 20:35:48 +0900

    path

--- src/pages/Board.py
+++ src/pages/Board.py
@@ -1,5 +1,9 @@
 import pygame
 import numpy as np
+import os 
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from widgets.pieces.King import King
 from widgets.pieces.Rook import Rook
